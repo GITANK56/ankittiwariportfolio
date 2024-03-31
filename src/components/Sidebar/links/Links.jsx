dee2a21c731ea816b3c1c0caca6754f303df56ac
Ankit tiwari <TIWARIANKI70@GMAIL.COM>
2024-03-31 21:45:44 +0530

    portfolio

--- src/components/Sidebar/links/Links.jsx
+++ src/components/Sidebar/links/Links.jsx
@@ -1,41 +1,47 @@
 import { motion } from "framer-motion";
 
 const variants = {
-    open: {
-        transition: {
-            staggerChilderen: 0.1,
-        },
+  open: {
+    transition: {
+      staggerChildren: 0.1,
     },
-    closed: {
-        transition: {
-            staggerChilderen: 0.05,
-            staggerDirection: -1,
-        },
+  },
+  closed: {
+    transition: {
+      staggerChildren: 0.05,
+      staggerDirection: -1,
     },
+  },
 };
 const itemVariants = {
-    open: {
-        y: 0,
-        opacity:1,
-    },
-    closed: {
-        y: 50,
-        opacity:0,
-    },
-};
-const Links = () =>{
-    const items =["Homepage", "Skills", "Portfolio", "About", "Contact"];
-
-
-    return(
-        <motion.div className="links" variants={variants}>
-            {items.map((item) => 
-            (<motion.a href={`#${item}`} key={item} variants={itemVariants} whileHover={{scale:1.1}} whileTap={{scale: 0.95}}>
-                {item}
-            </motion.a>
-          ))}
-        </motion.div>
-    );
+  open: {
+    y: 0,
+    opacity: 1,
+  },
+  closed: {
+    y: 50,
+    opacity: 0,
+  },
 };
 
-export default Links
+const Links = () => {
+  const items = ["Homepage", "Skills", "Portfolio", "Contact", "About"];
+
+  return (
+    <motion.div className="links" variants={variants}>
+      {items.map((item) => (
+        <motion.a
+          href={`#${item}`}
+          key={item}
+          variants={itemVariants}
+          whileHover={{ scale: 1.1 }}
+          whileTap={{ scale: 0.95 }}
+        >
+          {item}
+        </motion.a>
+      ))}
+    </motion.div>
+  );
+};
+
+export default Links;
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -13,8 +13,8 @@ const Navbar = () => {
                 >Ankit Tiwari</motion.span>
                 <div className="social">
                     <a href = "https://www.linkedin.com/in/ankit01tiwari"><img src="/linkedin (3).png" alt="" /></a>
-                    <a herf = "#"><img src="/github (2).png" alt="" /></a>
-                    <a herf = "#"><img src="/instagram.png" alt="" /></a>
+                    <a href = "https://github.com/GITANK56"><img src="/github (2).png" alt="" /></a>
+                    <a href = "https://www.instagram.com/_.ankit022/"><img src="/instagram.png" alt="" /></a>
                 </div>
             </div>
         </div>
